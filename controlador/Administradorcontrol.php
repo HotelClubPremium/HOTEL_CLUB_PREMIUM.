762659007d3e3f89c4bb9b36314084cd529bed47
migueljose <migueljose.gonzalez@hotmail.com>
2013-06-03 17:17:14 -0500

    versio 5.1

---
+++ controlador/Administradorcontrol.php
@@ -0,0 +1,40 @@
+<?php
+
+
+/**
+ * Clase para manejar los controladores en la aplicacion
+ *
+ * @author MELLADO
+ */
+class Administradorcontrol extends Controlador{
+    //put your code here
+    
+    public function __construct($modelo, $accion) {
+        parent::__construct($modelo, $accion);
+       
+    }
+
+ public function index() {
+        try {
+            /* session_start();
+            if (!isset($_SESSION['usuario.id'])) {
+                $this->setVista('fuera');
+                return $this->vista->imprimir();
+            }*/
+           
+          
+            $this->vista->set('titulo', 'Lista de usuarios');
+              $this->vista->set('Principal','');
+            return $this->vista->imprimir();
+        } catch (Exception $exc) {
+            echo 'Error de aplicacion: ' . $exc->getMessage();
+        }
+    }
+    
+    
+    
+    
+    
+}
+
+?>

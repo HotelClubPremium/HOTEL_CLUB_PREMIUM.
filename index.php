0937d18c30a42578b4eefc35ea121a4eccb4e67c
jeiner mellado <je_in_er@hotmail.com>
2013-06-09 22:42:56 -0500

    version 9.1

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ function cargadorClases(){
     require_once './MODELOS/Reserva.php';
     require_once './MODELOS/Facturacion.php'; 
     require_once './MODELOS/Tarjeta_registro.php';
+    require_once './MODELOS/Checkin.php'; 
     require_once './controlador/Controlador.php';
     require_once './controlador/Usuariocontrol.php';
     require_once './controlador/Habitacioncontrol.php';
@@ -34,6 +35,7 @@ function cargadorClases(){
     require_once './controlador/Facturacioncontrol.php';
     require_once './controlador/Principalcontrol.php';
     require_once './controlador/Tarjeta_registrocontrol.php';
+    require_once './controlador/Checkincontrol.php';
     require_once './utiles/class.phpmailer.php';
     require_once './utiles/class.pop3.php';
     require_once './utiles/class.smtp.php';
